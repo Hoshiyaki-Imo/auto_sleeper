ab9a0cb4733b6f96ac91982b93a0d92e7a333868
Hoshiyaki-Imo <watatyan0729@gmail.com>
2026-03-13 16:50:04 +0900

    v1.6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pystray, time, threading, os, tomllib, sys
+import pystray, time, threading, os, tomllib, sys, subprocess
 from PIL import Image
 
 SLEEPTIME = 10 #default value
@@ -23,7 +23,6 @@ def sleepReturnCheck():
         now = time.time()
         if now - last > 10:
             setSleepTime()
-            print("returned")
         last = now
         time.sleep(1)
         if timer and timer.is_alive():
@@ -66,17 +65,14 @@ def extendSleepTimer(icon, extend_minutes_item):
 
     makeTimer(new_time)
 
-    print("extended"+ str(extend_minutes) + "min")
-
 def makeTimer(sleep_time):
     global timer
     if timer:
         timer.cancel()
-        print()
     timer = threading.Timer(sleep_time, sleep)
     timer.start()
     icon.icon = default_image
-    print("make new timer")
+    icon.menu = makeIcon()
 
 def exit(icon = None, query = None):
     global running
@@ -89,17 +85,14 @@ def exit(icon = None, query = None):
 def cancelTimer(icon, query):
     if timer:
         timer.cancel()
-        print(timer)
     icon.icon = not_started_image
+    icon.menu = makeIcon()
 
 def openSettingFile(icon, query):
     if not os.path.exists(settingFilePath):
         with open(settingFilePath, "w") as f:
-            f.write("sleeping_time = 10\nextend_time = [5,10]\ntimer_time = [5]\n")
+            f.write("sleeping_time = 10\nextend_time = [5]\ntimer_time = [5]\n")
     os.startfile(settingFilePath)
-
-def restart():
-    os.execv(sys.executable, ['python'] + sys.argv)
 
 def getSettingPath():
     appdata = os.environ.get("APPDATA")
@@ -108,8 +101,10 @@ def getSettingPath():
         os.makedirs(app_dir)
     return os.path.join(app_dir, "setting.toml")
 
-def main():
-    global sleepReturn, SLEEPTIME, icon, settingFilePath
+def makeIcon():
+    global SLEEPTIME, settingFilePath
+    extendTime = None
+    timerTime = None
     settingFilePath = getSettingPath()
     try:
         with open(settingFilePath, mode="rb") as f:
@@ -120,22 +115,30 @@ def main():
     except:
         SLEEPTIME = 10
         extendTime = [5]
-        timerTime = []
+        timerTime = [5]
         print("not found setting file")
-    print("sleeping time set " + str(SLEEPTIME))
-    extendTimeMenuItems = [pystray.MenuItem(f"+{i}min", extendSleepTimer) for i in extendTime]
-    icon = pystray.Icon("auto_sleeper", default_image, "Auto Sleeper",
-                        menu=pystray.Menu(pystray.MenuItem("Wanna exit", pystray.Menu(pystray.MenuItem("Really?", pystray.Menu(pystray.MenuItem("No", None), pystray.MenuItem("Exit", exit))))),
+    
+    new_menu = pystray.Menu(pystray.MenuItem("Wanna exit", pystray.Menu(pystray.MenuItem("Really?", pystray.Menu(pystray.MenuItem("No", None), pystray.MenuItem("Exit", exit))))),
                                           pystray.MenuItem("Open setting file", openSettingFile),
-                                          pystray.MenuItem("Restart", restart),
+                                          pystray.MenuItem("Scan", scan),
                                           pystray.MenuItem("Cancel timer", cancelTimer, enabled= lambda _: timer and timer.is_alive()),
                                           pystray.MenuItem("Start timer", setSleepTime, default=True, enabled= lambda _: not (timer and timer.is_alive())),
                                           pystray.MenuItem("Start timer(select time)", pystray.Menu(*[pystray.MenuItem(f"{i}min", setSleepTime) for i in timerTime]), enabled = True if (len(timerTime) != 0 and not(timer and timer.is_alive())) else False),
-                                          pystray.MenuItem("Extend time", pystray.Menu(*extendTimeMenuItems)), 
-                                          )
+                                          pystray.MenuItem("Extend time", pystray.Menu(*[pystray.MenuItem(f"+{i}min", extendSleepTimer) for i in extendTime])),
+    )
+    return new_menu
+
+def scan(icon):
+    icon.menu = makeIcon()
+
+def main():
+    global sleepReturn, icon
+    icon = pystray.Icon("auto_sleeper", default_image, "Auto Sleeper",
+                        menu=None
     )
     sleepReturn = threading.Thread(target=sleepReturnCheck, daemon=True)
     sleepReturn.start()
+    icon.menu = makeIcon()
     icon.run()
 
 if __name__ == "__main__":
